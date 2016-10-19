8602cf9ae2469e2943345c98ad5cad3593bf7ffe
Tanmay Patel <tanmay.patel@ge.com>
2016-10-19 16:34:53 +0400

    Added configuration for passing client id, secret and signing secret via Environemnt Variables

--- config/default.js
+++ config/default.js
@@ -4,10 +4,5 @@ const appEnv = cfenv.getAppEnv();
 
 module.exports = {
     host: '0.0.0.0',
-    port: appEnv.port || 9999,
-    auth: {
-        clientId: '__TEST_CLIENT_ID__',
-        clientSecret: '__TEST_CLIENT_SECRET__',
-        secret: 'TOKEN_SIGNING_SECRET'
-    }
+    port: appEnv.port || 9999
 };
--- config/production.js
+++ config/production.js
@@ -4,6 +4,10 @@ const appEnv = cfenv.getAppEnv();
 
 const POSTGRES_CF_SERVICE_NAME = process.env.POSTGRES_CF_SERVICE_NAME;
 
+const CLIENT_ID = process.env.CLIENT_ID || '__TING_CLIENT_ID__';
+const CLIENT_SECRET = process.env.CLIENT_SECRET || '__TING_CLIENT_SECRET__';
+const TOKEN_SIGNING_SECRET = process.env.TOKEN_SIGNING_SECRET || '__TING_TOKEN_SIGNING_SECRET__';
+
 let postgresCredentials = null;
 if(POSTGRES_CF_SERVICE_NAME)
 {
@@ -11,7 +15,15 @@ if(POSTGRES_CF_SERVICE_NAME)
 }
 
 module.exports = {
+    auth: {
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        secret: TOKEN_SIGNING_SECRET
+    },
     dataStore: {
+        sqlite: {
+            filename: './data.sqlite3'
+        },
         postgres: {
             host: postgresCredentials.host,
             port: postgresCredentials.port,
